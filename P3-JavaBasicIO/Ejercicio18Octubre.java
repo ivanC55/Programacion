02488b2e87afbccbb006269fa75980cccd290dd9
Ivan <dawicerrosb@iescastelar.com>
2022-11-03 20:23:42 +0100

    Tarea 3

--- P3-JavaBasicIO/Ejercicio18Octubre.java
+++ P3-JavaBasicIO/Ejercicio18Octubre.java
@@ -1,7 +1,14 @@
-import Scanner;
+import java.util.Scanner;
 public class Ejercicio18Octubre{
 public static void main(String args[]){
-Scanner sc = new Scanner (System.in);
-
+	Scanner sc = new Scanner (System.in);
+	int op1=0;
+	int op2=0;
+	System.out.println("Dime el primer numero : ");
+	op1=sc.nextInt();
+	System.out.println("Dime el segundo numero : ");
+	op2=sc.nextInt();
+	System.out.println("La division es : "+(op1/op2));
+	System.out.println("El resto de la divisione es : "+(op1%op2));
 }
 }
