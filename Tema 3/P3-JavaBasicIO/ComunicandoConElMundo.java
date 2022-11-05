4d9bfb799c77c8afd037a4df722b1b46718e490f
Ivan <dawicerrosb@iescastelar.com>
2022-11-05 16:08:15 +0100

    Añado captura

--- Tema 3/P3-JavaBasicIO/ComunicandoConElMundo.java
+++ Tema 3/P3-JavaBasicIO/ComunicandoConElMundo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Ejercicio18Octubre{
+public class ComunicandoConElMundo{
 public static void main(String args[]){
 	Scanner sc = new Scanner (System.in);
 	int op1=0;
---
+++ Tema 3/P3-JavaBasicIO/Readme.md
@@ -0,0 +1,2 @@
+Autor: Ivan Cerros Brioso
+![capturaComunicandoConElMundo](https://user-images.githubusercontent.com/114107549/200126302-61d8c240-61e1-4711-96fc-6ca11a4d7e32.png)
