15c9c0461294de84fb46ee8c23952091e79c7b8d
ivanC55 <114107549+ivanC55@users.noreply.github.com>
2023-03-09 15:06:23 +0100

    herencia

---
+++ Herencia/src/Directivo.java
@@ -0,0 +1,16 @@
+//Clase Directivo. Clase derivada de la clase Empleado
+public class Directivo extends Empleado{
+
+    public Directivo() {
+    }
+
+    public Directivo(String nombre) {
+        super(nombre);
+        System.out.println("Constructor de Directivo");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " -> Directivo";
+    }
+}
---
+++ Herencia/src/Tecnico.java
@@ -0,0 +1,15 @@
+public class Tecnico extends Operario{
+
+    public Tecnico() {
+    }
+
+    public Tecnico(String nombre) {
+        super(nombre);
+        System.out.println("Constructor de Tecnico");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " -> Tecnico";
+    }
+}
